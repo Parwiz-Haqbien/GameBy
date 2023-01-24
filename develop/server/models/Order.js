b1c5e2ad619264a87fe62a597e272cd0d4f27538
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-24 18:33:31 +1100

    added Order model

---
+++ develop/server/models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+});
+
+  const Order = mongoose.model('Order', orderSchema);
+
+  module.exports = Order;
