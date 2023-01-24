d1fb4940de823e0324acd5fa52773685e114cbfb
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-24 18:57:04 +1100

    added product schema

--- develop/server/models/Product.js
+++ develop/server/models/Product.js
@@ -2,8 +2,35 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+  // Setting the criteria for product that will be displayed in the page
 const productSchema = new Schema({
     name: {
-        
+       type: String,
+       required: true,
+       trim: true 
+    },
+    
+    description: {
+        type: String
+    },
+
+    image: {
+        type: String
+    },
+
+    price: {
+        type: Number,
+        required: true,
+        min: 0.99
+    },
+
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
     }
-})
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
