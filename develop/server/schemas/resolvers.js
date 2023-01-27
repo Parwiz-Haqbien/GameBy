517b4af1bd1ff6b5d671dfcfa9f9ac985613a660
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-27 17:03:01 +1100

    added the checkout

--- develop/server/schemas/resolvers.js
+++ develop/server/schemas/resolvers.js
@@ -23,13 +23,13 @@ const resolvers = {
               };
             }
             return await Product.find(filterProduct).populate('category');
-    },
+        },
         //To return the result 
         product: async (parent, { _id }) => {
         return await Product.findById(_id).populate('category');
         },
-        user: async (parent, args, context) => {
-            if (context.user) {
+            user: async (parent, args, context) => {
+             if (context.user) {
               const user = await User.findById(context.user._id).populate({
                 path: 'orders.products',
                 populate: 'category'
@@ -42,8 +42,8 @@ const resolvers = {
       
             throw new AuthenticationError('Not logged in');
           },
-          order: async (parent, { _id }, context) => {
-            if (context.user) {
+            order: async (parent, { _id }, context) => {
+              if (context.user) {
               const user = await User.findById(context.user._id).populate({
                 path: 'orders.products',
                 populate: 'category'
@@ -54,7 +54,53 @@ const resolvers = {
       
             throw new AuthenticationError('Not logged in');
           },
-         
-}
+          checkout: async (parent, args, context) => {
+               const url = new URL(context.headers.referer).origin;
+               const order = new Order({ products: args.products });
+               const line_items = [];
+
+               const { products } = await order.populate('products');
+               for (let i = 0; i < products.length; i++) {
+                //Create a PayPal payment object
+                const payment = await PayPal.payment.create({
+                  intent: 'sale',
+                  payer: {
+                    payment_method: 'paypal'
+                  },
+                  transactions: [{
+                    item_list: {
+                      items: [{
+                        name: products[i].name,
+                        description: products[i].description,
+                        quantity: 1,
+                        price: products[i].price,
+                        currency: 'AUD'
+                      }]
+                    },
+                    amount: {
+                      currency: 'AUD',
+                      total: products[i].price
+                    }
+                  }],
+                  redirect_urls: {
+                    return_url: `${url}/success`,
+                    cancel_url: `${url}/`
+                  }
+                });
+            
+                //Add the payment ID to the line_items array
+                line_items.push({
+                  payment: payment.id,
+                });
+              }
+            
+              //Execute the PayPal payment
+              const execution = await PayPal.payment.execute(line_items[0].payment, { payer_id: payerId });
+            
+              //Return the payment ID
+              return { payment: execution.id };
+            }
+
+    }
 
 }
