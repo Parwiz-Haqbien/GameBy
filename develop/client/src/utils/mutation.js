8c4fc5f79051ddd94916a57946292c3ee4af98df
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-02-03 00:53:24 +1100

    added mutation

---
+++ develop/client/src/utils/mutation.js
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_ORDER = gql`
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      purchaseDate
+      products {
+        _id
+        name
+        description
+        price
+        category {
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $userName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      userName: $userName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
