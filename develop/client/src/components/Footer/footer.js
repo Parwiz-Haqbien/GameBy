2e182d28d52324689315e24b09e597c87b861adb
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-02-02 18:30:38 +1100

    added the footer

--- develop/client/src/components/Footer/footer.js
+++ develop/client/src/components/Footer/footer.js
@@ -6,7 +6,34 @@ const Footer = () => {
     <div className="main-footer">
         <div className="container">
            <div className="row">
-
+            {/* Column1*/}
+            <div className="col1">
+              <h4>GameBy INC</h4>
+               <ul className="list-unstyled">
+                <li>964-342-110</li>
+                <li>Melbourne, Australia</li>
+               </ul>
+            </div>
+            {/* Column2*/}
+            <div className="col">
+              <h4>STUFF</h4>
+               <ul className="list-unstyled">
+               <li>Parwiz Haqbien</li>
+               </ul>
+            </div>
+            {/* Column3*/}
+            <div className="col">
+              <h4>Contact Me</h4>
+               <ul className="list-unstyled">
+               <li>parwiz.jan03@gmail.com</li>
+               </ul>
+            </div>
+           </div>
+           <hr />
+           <div className="row">
+            <p className="col-sm">
+              &copy;{new Date().getFullYear()} GameBy INC | All right reserved | Terms Of Services | Privacy
+            </p>
            </div>
         </div>
     </div>
