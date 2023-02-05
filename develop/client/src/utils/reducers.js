b101be957487c4fa9e3dd05a79fe9b952df3ab8a
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-02-05 23:14:40 +1100

    fixed the reducers

--- develop/client/src/utils/reducers.js
+++ develop/client/src/utils/reducers.js
@@ -1,21 +1,21 @@
 import { useReducer } from "react";
 import {
+  UPDATE_PRODUCTS,
   ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
   REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
   CLEAR_CART,
-  TOGGLE_CART_VISIBILITY,
-  REPLACE_PRODUCTS
-} from "./index";
+  TOGGLE_CART
+} from "./actions";
 
 export const reducer = (state, action) => {
   switch (action.type) {
-
-    case REPLACE_PRODUCTS:
+    case UPDATE_PRODUCTS:
       return {
         ...state,
         products: [...action.products],
       };
-
 
     case ADD_TO_CART:
       return {
@@ -23,7 +23,25 @@ export const reducer = (state, action) => {
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
-    
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map(product => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity
+          }
+          return product
+        })
+      };
+
     case REMOVE_FROM_CART:
       let newState = state.cart.filter(product => {
         return product._id !== action._id;
@@ -42,11 +60,12 @@ export const reducer = (state, action) => {
         cart: []
       };
 
-    case TOGGLE_CART_VISIBILITY:
+    case TOGGLE_CART:
       return {
         ...state,
         cartOpen: !state.cartOpen
       };
+
     default:
       return state;
   }
